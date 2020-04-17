330787851820a8e0a94d26a051a692fbd1405dd8
tismenetski <tismenetski@gmail.com>
2020-04-17 22:30:22 +0300

    * Added The React Part

---
+++ src/main/resources/static/precache-manifest.e2c80bc999001d7efce0215cbfa18881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641479af83d951e6dc4e924febd1dcdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f414197ed5a4120f72b1",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d408e9b53033741076d9",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "f414197ed5a4120f72b1",
+    "url": "/static/js/2.d7395ba2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d7395ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d408e9b53033741076d9",
+    "url": "/static/js/main.966000f7.chunk.js"
+  },
+  {
+    "revision": "c0b1b3110e1933f80ca8",
+    "url": "/static/js/runtime-main.4de9df80.js"
+  }
+]);
